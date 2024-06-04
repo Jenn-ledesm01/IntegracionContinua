7bcaef6bcf51e64ba78bc3644b37048eb632a3e2
Janaina Bacin <114409955+janaBR30@users.noreply.github.com>
2024-06-04 19:21:45 -0300

    Create Circle and CircleTest

---
+++ test/FigurasGeometricas/CirculoTest.java
@@ -0,0 +1,28 @@
+package FigurasGeometricas;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CirculoTest {
+
+    @Test
+    void validadarAreaCirculo() {
+        double resultado = Circulo.area(4);
+        double expected = Math.PI * Math.pow(4, 2);
+        assertEquals(expected, resultado, 0.0001);
+    }
+
+    @Test
+    void validadarPerimetroCirculo() {
+        double resultado = Circulo.perimetro(5);
+        double expected = 2 * Math.PI * 5;
+        assertEquals(expected, resultado, 0.0001);
+    }
+
+    @Test
+    void validadarVolumenCirculo() {
+        double resultado = Circulo.volumen(4);
+        double expected = (4/3.0) * Math.PI * Math.pow(4, 3);
+        assertEquals(expected, resultado, 0.0001);
+    }
+}
