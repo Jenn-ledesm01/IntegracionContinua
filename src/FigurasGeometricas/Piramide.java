b3e303c7123649c0ce3395faadc1591b8e0150f0
Luu-maldonado <110115736+Luu-maldonado@users.noreply.github.com>
2024-06-04 20:43:47 -0300

    codigo y test de piramides

---
+++ src/FigurasGeometricas/Piramide.java
@@ -0,0 +1,17 @@
+package FigurasGeometricas;
+
+public class Piramide {
+
+    public static double areaBase(double lado) {
+        return lado * lado;
+    }
+
+    public static double perimetroBase(double lado) {
+        return 4 * lado;
+    }
+
+    public static double volumen(double lado, double altura) {
+        return (1.0 / 3.0) * areaBase(lado) * altura;
+    }
+}
+
---
+++ test/FigurasGeometricas/PiramideTest.java
@@ -0,0 +1,26 @@
+package FigurasGeometricas;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PiramideTest {
+
+    @Test
+    void validarAreaBasePiramide() {
+        double resultado = Piramide.areaBase(4.0);
+        assertEquals(16.0, resultado, 0.001);
+    }
+
+    @Test
+    void validarPerimetroBasePiramide() {
+        double resultado = Piramide.perimetroBase(5.0);
+        assertEquals(20.0, resultado, 0.001);
+    }
+
+    @Test
+    void validarVolumenPiramide() {
+        double resultado = Piramide.volumen(4.0, 6.0);
+        assertEquals(32.0, resultado, 0.001);
+    }
+}
