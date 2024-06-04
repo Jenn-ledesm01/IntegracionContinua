44f754e96d435a75435ec68eb522cf2b04ec26a6
Jenn-ledesm01 <87781908+Jenn-ledesm01@users.noreply.github.com>
2024-06-04 18:39:42 -0300

    add test

--- test/FigurasGeometricas/RectanguloTest.java
+++ test/FigurasGeometricas/RectanguloTest.java
@@ -11,10 +11,12 @@ class RectanguloTest {
         int resultado = Rectangulo.area(3,4);
         assertEquals(resultado, 12);
     }
+    @Test
     void validarPerimetroRectangulo(){
         int resultado = Rectangulo.perimetro(3,4);
         assertEquals(resultado, 14);
     }
+    @Test
     void validarVolumenRectangulo(){
         int resultado = Rectangulo.volumen(3,4,5);
         assertEquals(resultado,60 );
