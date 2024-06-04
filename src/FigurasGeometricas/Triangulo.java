92c729a975bb719348174d8f4a27c10981a103e4
Facundo Condori <71018977+FacuCe@users.noreply.github.com>
2024-06-04 20:25:13 -0300

    triangulo (#3)

---
+++ src/FigurasGeometricas/Triangulo.java
@@ -0,0 +1,11 @@
+package FigurasGeometricas;
+
+public class Triangulo {
+    public static double area(double base, double altura) { return base * altura / 2; }
+
+    public static double perimetro(double lado1, double lado2, double lado3) { return lado1 + lado2 + lado3; }
+
+    public static double hipotenusa(double lado1, double lado2) {
+        return Math.sqrt(Math.pow(lado1, 2) + Math.pow(lado2, 2));
+    }
+}
---
+++ test/FigurasGeometricas/TrianguloTest.java
@@ -0,0 +1,26 @@
+package FigurasGeometricas;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TrianguloTest {
+    @Test
+    void validarAreaTriangulo() {
+        double resultado = Triangulo.area(4, 5);
+        double expected = (double) (4 * 5) / 2;
+        assertEquals(expected, resultado);
+    }
+
+    @Test
+    void validarPerimetroTriangulo() {
+        double resultado = Triangulo.perimetro(2, 3, 4);
+        double expected = 2 + 3 + 4;
+        assertEquals(expected, resultado);
+    }
+
+    @Test
+    void validarHipotenusa() {
+        double resultado = Triangulo.hipotenusa(4, 7);
+        double expected = Math.sqrt(Math.pow(4, 2) + Math.pow(7, 2));
+        assertEquals(expected, resultado);
+    }
+}
