7229da49803e52b9560485ea6f502a72497b6f35
Jenn-ledesm01 <87781908+Jenn-ledesm01@users.noreply.github.com>
2024-06-08 20:31:50 -0300

    added diffplug spotless

--- src/test/java/org/example/FigurasGeometricas/CirculoTest.java
+++ src/test/java/org/example/FigurasGeometricas/CirculoTest.java
@@ -1,28 +1,28 @@
 package org.example.FigurasGeometricas;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CirculoTest {
+
     @Test
-    void validadarAreaCirculo() {
+    void validarAreaCirculo() {
         double resultado = Circulo.area(4);
         double expected = Math.PI * Math.pow(4, 2);
         assertEquals(expected, resultado, 0.0001);
     }
 
     @Test
-    void validadarPerimetroCirculo() {
+    void validarPerimetroCirculo() {
         double resultado = Circulo.perimetro(5);
         double expected = 2 * Math.PI * 5;
         assertEquals(expected, resultado, 0.0001);
     }
 
     @Test
-    void validadarVolumenEsfera() {
+    void validarVolumenEsfera() {
         double resultado = Circulo.volumen(4);
-        double expected = (4 / 3.0) * Math.PI * Math.pow(4, 3);
+        double expected = (4/3.0) * Math.PI * Math.pow(4, 3);
         assertEquals(expected, resultado, 0.0001);
     }
 }
