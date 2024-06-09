6d63bcad91f8c561a6c027a4a5838e388d8ca8fe
Jenn-ledesm01 <87781908+Jenn-ledesm01@users.noreply.github.com>
2024-06-08 21:09:56 -0300

    added checkstyle

--- src/main/java/org/example/FigurasGeometricas/Cubo.java
+++ src/main/java/org/example/FigurasGeometricas/Cubo.java
@@ -1,15 +1,16 @@
 package org.example.FigurasGeometricas;
 
 public class Cubo {
-    public static Integer areaUnaCara(Integer lado){
+    public static Integer areaUnaCara(Integer lado) {
         return lado * lado;
     }
 
-    public static Integer perimetroUnaCara(Integer lado){
+    public static Integer perimetroUnaCara(Integer lado) {
         return 4 * lado;
     }
 
-    public static Integer volumen(Integer lado){
+    public static Integer volumen(Integer lado) {
         return lado * lado * lado;
     }
 }
+
